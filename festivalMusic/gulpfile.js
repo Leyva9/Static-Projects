afe7599680296206c151b5504585a44099decac1
Leyva9 <leyva0919@gmail.com>
2023-10-17 19:44:42 -0400

    postproduction js done

--- festivalMusic/gulpfile.js
+++ festivalMusic/gulpfile.js
@@ -2,19 +2,29 @@ const { src, dest, watch, parallel} = require("gulp");
 //CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
 //Images
 const cache = require("gulp-cache");
 const webp = require("gulp-webp");
 const imagemin = require("gulp-imagemin");
 const avif = require("gulp-avif");
 
+//JavaScript
+const terser = require("gulp-terser-js");
+
 // CSS
 function css( cb ) {   
     src('src/scss/**/*.scss') //Identificar el archivo de SASS
+        .pipe(sourcemaps.init()) // Guarda las referencias de los archivos css
         .pipe( plumber() ) //Se utiliza para que no se detenga el watch en caso de que haya error en la compilacion
         .pipe( sass() )  //Compilarlo
-        .pipe( dest('build/css') ); //Guardarlo en el disco duro
-
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.')) // Ubicacion donde se van a guardar las referecias
+        .pipe( dest('build/css') ) //Guardarlo en el disco duro
     cb(); // Esto es un call back que se utiliza para decirle a gulp que esta funcion llego al final
 }
 
@@ -61,6 +71,9 @@ function imgAvif(cb) {
 // JavaScript
 function javascript(cb) {
     src('./src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('./build/js'));
     cb();
 }
